fdec9762ca1064f535726d375d404bf8ead74beb
Vinicius Dietrich <vin.dietrich@gmail.com>
2020-01-29 16:51:19 -0300

    Adicionada a aba produto

--- CursoMVC/Models/Produto.cs
+++ CursoMVC/Models/Produto.cs
@@ -8,7 +8,7 @@ namespace CursoMVC.Models
     public class Produto
     {
         public int Id { get; set; }
-        public string Descricao { get; set; 
+        public string Descricao { get; set; }
         public int Quantidade { get; set; }
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
