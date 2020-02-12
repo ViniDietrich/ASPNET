e621da5f512f3da37fd92c35b8c5822dcc0ae29c
Vinicius Dietrich <vin.dietrich@gmail.com>
2020-02-12 16:47:42 -0300

    Testes Dot NET

---
+++ CursoTest/CategoriaControllerTEst.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CursoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace CursoTest
+{
+    public class CategoriaControllerTest
+    {
+        private readonly Mock<DbSet<Categoria>> _mockset;
+        private readonly Mock<Context> _mockContext;
+        private readonly Categoria _categoria;
+
+        public CategoriaControllerTest()
+        {
+            _mockset = new Mock<DbSet<Categoria>>();
+            _mockContext = new Mock<Context>;
+            _categoria = new Categoria { id = 1, Descricao = "Teste Categoria" };
+        }
+    }
+}
